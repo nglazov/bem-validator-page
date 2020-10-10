a05692ad41e33332ef090ccce4936511d589910a
Nikita Glazov <nglazov@gmail.com>
2020-10-10 22:54:15 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,120 @@
+const button = document.querySelector("button");
+if (button) {
+  button.addEventListener("click", validate);
+}
+
+// console.log = ()=>{}
+const ul = document.querySelector("ul");
+
+function validate() {
+  ul.innerHTML = "";
+  const text = document.querySelector("textarea").value;
+  var div = document.createElement("div");
+  div.innerHTML = text.trim();
+
+  if (!text || !div.children[0]) {
+    insertErrors();
+    return;
+  }
+
+  const errors = validateNode(div.children[0]);
+
+  insertErrors(errors);
+}
+
+function insertErrors(errors) {
+  if (!errors.length) {
+    errors.push("No errors");
+  }
+
+  errors.forEach((error) => {
+    const li = document.createElement("li");
+    li.innerText = error;
+    ul.appendChild(li);
+  });
+}
+
+function validateNode(node, parentArray = []) {
+  const errors = [];
+  const currentClasses = [...node.classList];
+  const parentArrayWithCurent = [...parentArray, currentClasses];
+
+  currentClasses.forEach((className) => {
+    const { blockName, elementName, modifierName } = parseClassName(className);
+
+    if (
+      elementName &&
+      !parentArray.flat().find((parentClass) => parentClass === blockName)
+    ) {
+      errors.push(
+        `Element without block in parents (element: ${className}, path: ${getParentPath(
+          parentArray
+        )})`
+      );
+    }
+
+    if (
+      elementName &&
+      parentArray
+        .flat()
+        .find((parentClass) => parentClass === `${blockName}__${elementName}`)
+    ) {
+      errors.push(
+        `Element is in element with same name (element: ${className}, path: ${getParentPath(
+          parentArray
+        )})`
+      );
+    }
+
+    if (
+      modifierName &&
+      !currentClasses.find((currentClass) =>
+        elementName
+          ? currentClass === `${blockName}__${elementName}`
+          : blockName === currentClass
+      )
+    ) {
+      errors.push(
+        `Modifier without block or element in classList (modifier: ${className}, path: ${getParentPath(
+          parentArray
+        )})`
+      );
+    }
+  });
+
+  if (node.children) {
+    [...node.children].forEach((node) => {
+      const childErrors = validateNode(node, parentArrayWithCurent);
+      errors.push(...childErrors);
+    });
+  }
+
+  return errors;
+}
+
+function getParentPath(parents) {
+  return parents
+    .map((element) => (Array.isArray(element) ? element.join(".") : element))
+    .join(" > ");
+}
+
+function parseClassName(className) {
+  const regExp = /^([a-z-0-9]*)(__)?([a-z-0-9]*)(_)?([a-z-0-9]*)?(_)?([a-z-0-9])?/i;
+
+  const [
+    ,
+    blockName,
+    ,
+    elementName,
+    ,
+    modifierName,
+    ,
+    modifierValue
+  ] = regExp.exec(className);
+
+  return { blockName, elementName, modifierName, modifierValue };
+}
+
+module.exports = {
+  validateNode
+}
---
+++ src/index.test.js
@@ -0,0 +1,70 @@
+const { validateNode } = require("./index");
+
+describe("validateNode", () => {
+  it("returns error when modifier used without block or element", () => {
+    const div = createDiv(`
+      <div class="block_modifier"></div>
+    `);
+
+    const errors = validateNode(div);
+
+    expect(errors.length).toBe(1);
+    expect(errors[0]).toBe(
+      "Modifier without block or element in classList (modifier: block_modifier, path: )"
+    );
+  });
+
+  it("returns error when element has no block in parents", () => {
+    const div = createDiv(`
+    <div class="block__element"></div>
+  `);
+
+    const errors = validateNode(div);
+
+    expect(errors.length).toBe(1);
+    expect(errors[0]).toBe(
+      "Element without block in parents (element: block__element, path: )"
+    );
+  });
+
+  it("returns error when element has same element as parent", () => {
+    const div = createDiv(`
+    <div class="block">
+      <div class="block__element">
+        <div class="block__element"></div>
+      </div>
+    </div>
+  `);
+
+    const errors = validateNode(div);
+
+    expect(errors.length).toBe(1);
+    expect(errors[0]).toBe(
+      "Element is in element with same name (element: block__element, path:  > block > block__element)"
+    );
+  });
+
+  it("does not return error for valid BEM", () => {
+    const div = createDiv(`
+    <div class="block">
+      <div class="block__element block__element_modifier">
+      </div>
+    </div>
+    <div class="block_modifier block"></div>
+    <div class="block-a-1">
+      <div class="block-b block-a-1__element"></div>
+    </div>
+  `);
+
+    const errors = validateNode(div);
+
+    expect(errors.length).toBe(0);
+  });
+});
+
+function createDiv(innerHtml) {
+  const div = document.createElement("div");
+  div.innerHTML = innerHtml;
+
+  return div;
+}
